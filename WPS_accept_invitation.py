b830f6bcb22935cf661116172e4072c2f6a8b31f
雪凌Yukiryo <58250223+yukiryo@users.noreply.github.com>
2021-03-16 12:24:02 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -20,13 +20,13 @@ sids = [
 ]
 mk = 0
 
-def request_re(sid, invite_userid, rep = 30):
+def request_re(sid, invite_userid, rep = 50):
     invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
     r = requests.post(invite_url, headers={'sid': sid}, data={'invite_userid': invite_userid})
     js = json.loads(r.content)
     if js['msg'] == 'tryLater' and rep > 0:
         rep -= 1
-        time.sleep(2)
+        time.sleep(5)
         r = request_re(sid, invite_userid, rep)
     return r
 
